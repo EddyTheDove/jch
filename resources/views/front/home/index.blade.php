acfe3b93c4549f3e96c6049b2ab58ff4c1590a53
EddyTheDove <eddy.insearch@gmail.com>
2018-04-01 19:32:19 +1000

    added seo keywords

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="robots" content="index,follow">
     <meta name="author" content="XMD Motors">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="Japanese car history">
+    <meta name="keywords" content="japanese car history, car history, japanese car, car history, car history check, car vin, vehicle report, vehicle checker, car reports, auto report, auto checker, identify car by vin, i car check, car history website, check number plate, carfax cars, hpi car check, car number plate check, vin number report, view car history, lookup car history, car history report">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" type="text/css" href="{{ mix('/assets/css/app.css') }}">
 
