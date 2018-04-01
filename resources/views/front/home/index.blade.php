5b6e9c7fa2587c6c7fbabf54a94b0e6660842de9
EddyTheDove <eddy.insearch@gmail.com>
2018-04-01 19:45:07 +1000

    added description meta

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -7,6 +7,7 @@
     <meta name="author" content="XMD Motors">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="japanese car history, car history, japanese car, car history, car history check, car vin, vehicle report, vehicle checker, car reports, auto report, auto checker, identify car by vin, i car check, car history website, check number plate, carfax cars, hpi car check, car number plate check, vin number report, view car history, lookup car history, car history report">
+    <meta name="description" content="Japanese odometer and accident history check. A world-wide service to verify the mileage and condition of any imported used Japanese vehicle.">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" type="text/css" href="{{ mix('/assets/css/app.css') }}">
 
